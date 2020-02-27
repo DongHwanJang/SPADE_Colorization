48ee0ab1601aba111a3af03cf9ef1a0effc57b36
hksong35 <hksong35@mindslab.ai>
2020-02-27 13:59:09 +0900

    add affin_dataset.py

---
+++ data/affin_dataset.py
@@ -0,0 +1,30 @@
+"""
+Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+
+import os.path
+from data.pix2pix_dataset import Pix2pixDataset
+from data.image_folder import make_dataset
+
+
+class AffinDataset(Pix2pixDataset):
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        parser = Pix2pixDataset.modify_commandline_options(parser, is_train)
+        parser.set_defaults(preprocess_mode='resize_and_crop')
+        # load_size = 286 if is_train else 256
+        load_size = 256 if is_train else 256  # FIXME : need to concern about input size
+        parser.set_defaults(load_size=load_size)
+        parser.set_defaults(crop_size=256)
+        parser.set_defaults(display_winsize=256)
+        parser.set_defaults(label_nc=13)
+        parser.set_defaults(contain_dontcare_label=False)
+        parser.set_defaults(no_instance=True)
+        return parser
+
+    def get_paths(self, opt):
+        make_dataset(opt.dataroot)
+
+        return pair_data
